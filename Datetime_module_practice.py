688b6ea278ef82fa45aad7e615a4624d9da29935
Giraffbutler <97141544+Giraffbutler@users.noreply.github.com>
2022-01-07 08:37:43 -0500

    First commit codecademy module practice

---
+++ Datetime_module_practice.py
@@ -0,0 +1,22 @@
+#Importing the 'datetime' module
+from datetime import datetime
+
+birthday = datetime(1998, 6, 7, 10, 21)
+#print(birthday.year)
+#print(birthday.weekday())
+
+#Using datetime.now()
+#print(datetime.now())
+
+#Using timedelta
+#print(datetime(2020, 6, 19, 8, 9) - datetime(2020, 3, 5, 18, 27))
+
+#parsing string to time --> https://docs.python.org/3/library/datetime.html
+parsed_date = datetime.strptime('Jan 15, 2018', '%b %d, %Y')
+#print(parsed_date.month)
+#print(parsed_date.year)
+#print(parsed_date.day)
+
+#creating string from time value
+date_string = datetime.strftime(datetime.now(), '%b %d, %Y')
+print(date_string)
---
+++ test.py
@@ -0,0 +1,30 @@
+"""
+my_info = ('Jake', 23, 'Chemical Engineer')
+
+name, age, occupation =my_info
+
+print(name, age, occupation)
+
+
+my_list = ['Jake', 23, 'Chemical Engineer']
+
+name, age, occupation = my_list
+
+print(name,age,occupation)
+"""
+
+
+lst1 = [0,1,2]
+lst2 = [4,3,5]
+
+lst3 = lst1 + lst2
+lst3.sort()
+print(lst3)
+
+lst = [0,2,2]
+popped = lst.pop(1)
+print (popped)
+print(lst[:2])
+
+test = []
+print(sum(test))
